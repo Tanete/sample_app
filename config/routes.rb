bbdbb9ce3e4f341f560545accee504cb6f09e69e
tanete <tanete772@outlook.com>
2016-08-12 10:48:01 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-
   root "static_page#home"
   get '/help', to: 'static_page#help'
   get '/about', to: 'static_page#about'
   get '/contact', to: 'static_page#contact'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
 end
