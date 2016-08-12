6e7bfca97db01c0c92091f5e48bbfe71e1994de1
tanete <tanete772@outlook.com>
2016-08-12 14:09:07 +0000

    Implement basic login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,24 @@
+module SessionsHelper
+  
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end  
+  
+  
+  def current_user
+    # impressive
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
+  
+
+end
