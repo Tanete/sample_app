4249c6c0269158e55aceebb320b941307812d9e6
tanete <tanete772@outlook.com>
2016-08-20 03:25:27 +0000

    fix host missing error for stmp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
+  host = 'calm-eyrie-44594.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :user_name => ENV['MAILTRAP_USERNAME'],
     :password => ENV['MAILTRAP_PASSWORD'],
