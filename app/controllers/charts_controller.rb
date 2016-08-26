3f4fd44c69d62153ce3220e252bd522ec6ba73fb
tanete <tanete772@outlook.com>
2016-08-26 13:33:06 +0000

    fix today chart error

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,12 +1,10 @@
 class ChartsController < ApplicationController
   
   @@start_date = ("2016-08-19").to_datetime
-  @@range = 8.hours.ago(@@start_date)..(8.hours.ago(Time.zone.today)-1)
-  @@range_today = 24.hours.ago(Time.zone.now)..Time.zone.now
   
   def mblog_today_count
     result = Mblog.group_by_hour(:created_timestamp,
-              time_zone = "Beijing", range: @@range_today, 
+              time_zone = "Beijing", range: get_range_today, 
               current: true, format: "%l %P").count
     render json: result
   end
@@ -23,17 +21,15 @@ class ChartsController < ApplicationController
   end
   
   def mblog_group_by_day
-    
-    
     result = Mblog.group_by_day(:created_timestamp, 
-                                  time_zone: 'Beijing', range: @@range).count
+                                  time_zone: 'Beijing', range: get_range).count
     render json: result
   end
   
   def mblog_group_by_hour_of_day
     days = (Date.today-@@start_date).to_i
     result = Mblog.group_by_hour_of_day(:created_timestamp, 
-                                  time_zone: 'Beijing', range: @@range).count
+                                  time_zone: 'Beijing', range: get_range).count
     result.each do |hour, count|
       result[hour] = count / days
     end
@@ -51,4 +47,15 @@ class ChartsController < ApplicationController
                                 .order("count_all DESC, source").limit(5).count
     render json: result
   end
+  
+  private
+    def get_range_today
+      start = 24.hours.ago(Time.zone.now).change(:min=>0) + 1.hour + 1.second
+      ending = Time.zone.now.change(:min=>0) + 1.hour- 1.second
+      range_today = start..ending
+    end
+    
+    def get_range
+      range = 8.hours.ago(@@start_date)..(8.hours.ago(Time.zone.today)-1)
+    end
 end
