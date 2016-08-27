d898c400c6419aeeb9276c12524edfe63f417441
tanete <tanete772@outlook.com>
2016-08-27 10:58:03 +0000

    Change host from heroku to insclone

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'calm-eyrie-44594.herokuapp.com'
+  host = 'rails.insclone.com'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :user_name => ENV['MAILTRAP_USERNAME'],
